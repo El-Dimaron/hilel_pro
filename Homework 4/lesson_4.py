c89f99e6ff9fa296a50743417fd304d918820cc2
El-Dimaron <el.dimaron.n1@gmail.com>
2025-03-21 23:53:14 +0200

    Corrected lesson_4.py: title method, query, added one more table to track search. Added db to .gitignore

--- Homework 4/lesson_4.py
+++ Homework 4/lesson_4.py
@@ -12,23 +12,20 @@ def order_price(country) -> str:
     """The function displays total sales value from chinook.db.
     If the Country value is specified, the function returns the total value for that country."""
 
-    if country:
-        if country.lower() == "usa":
-            country = "USA"
-        else:
-            country = country.title()
-        query = (f"SELECT BillingCountry, SUM(UnitPrice * Quantity) AS SALES FROM invoice_items LEFT JOIN invoices "
-                 f"ON invoice_items.InvoiceId = invoices.InvoiceId WHERE BillingCountry = '{country}'")
-    else:
-        query = "SELECT SUM(UnitPrice * Quantity) AS SALES FROM invoice_items"
-    records: list = execute_query(query)
+    query = (f"SELECT BillingCountry, SUM(UnitPrice * Quantity) AS SALES FROM invoice_items LEFT JOIN invoices "
+             f"ON invoice_items.InvoiceId = invoices.InvoiceId")
 
     if country:
+        country = country.title()
+        query += f" WHERE BillingCountry = '{country}'"
+        records: list = execute_query(query)
         country_name: str = records[0][0]
         country_sales: float = records[0][1]
         return f"{country_name} total sales: {country_sales}"
     else:
-        total_sales: float = records[0][0]
+        records: list = execute_query(query)
+        print(records)
+        total_sales: float = records[0][1]
         return f"Total sales: {total_sales}"
 
 
@@ -41,8 +38,12 @@ def get_all_info_about_track(track_id) -> list:
     """The function displays all the information about the track (including its time) when track_id is specified.
     If no track_id is specified, the function returns the list of all available tracks and their total time."""
 
-    query = ("SELECT * FROM tracks LEFT JOIN albums ON tracks.AlbumId = albums.AlbumId LEFT JOIN media_types on "
-                 "tracks.MediaTypeId = media_types.MediaTypeId LEFT JOIN genres ON tracks.GenreId = genres.GenreId")
+    query = ("SELECT * FROM tracks "
+             "LEFT JOIN albums ON tracks.AlbumId = albums.AlbumId "
+             "LEFT JOIN artists ON albums.ArtistId = artists.ArtistId "
+             "LEFT JOIN media_types on tracks.MediaTypeId = media_types.MediaTypeId "
+             "LEFT JOIN genres ON tracks.GenreId = genres.GenreId")
+
     if track_id:
         query += f" WHERE TrackId = {track_id}"
 
@@ -62,12 +63,7 @@ def get_all_info_about_track(track_id) -> list:
 
     return records
 
-    # show time of all tracks of all albums in hours
-    # use info about all tracks
-
 
 # http://127.0.0.1:5001/get-all-info-about-track
 
-
 app.run(port=5001, debug=True)
-
