efd6e14a41fa0c7d3317179b5513bb0207479cd0
El-Dimaron <el.dimaron.n1@gmail.com>
2025-05-01 14:39:45 +0300

    Homework 14

--- Homework 15/leetcode_2.py
+++
@@ -1,7 +0,0 @@
-# Link:
-# https://leetcode.com/problems/length-of-last-word/description/
-
-
-class Solution:
-    def lengthOfLastWord(self, s: str) -> int:
-        return len(s[::-1].split(maxsplit=1)[0][::-1].strip())
